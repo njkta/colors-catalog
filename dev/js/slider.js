0e781511709e2d29c0867953a7ac022bfc6bf1d1
njkta <njkta2405@gmail.com>
2022-08-26 19:09:16 +0300

    feat: add dev folder, gulpfile, adaptive etc.

---
+++ dev/js/slider.js
@@ -0,0 +1,10 @@
+const mainSlider = new Swiper('.main-slider', {
+    navigation: {
+        nextEl: ".btn-next",
+        prevEl: ".btn-prev",
+    },
+    pagination: {
+        el: ".main-slider__pagination",
+    },
+});
+
